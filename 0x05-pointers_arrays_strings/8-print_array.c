132bae8882ec62ef185310821d4b3facf181b059
xylevy <82037387+xylevy@users.noreply.github.com>
2022-12-20 05:35:29 -0800

    Task 8 betty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_array - prints n elements of an array of integers, followed by a new line.
+ * print_array - prints n elements of an array of integers
  * @a: pointer
  * @n: integer
  */
