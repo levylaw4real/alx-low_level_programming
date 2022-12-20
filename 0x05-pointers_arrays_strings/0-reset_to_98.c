d8ebebfee36ca7b910351f71a365659a48b225ed
xylevy <82037387+xylevy@users.noreply.github.com>
2022-12-20 00:33:55 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value n points to to 98
+ * @n:  pointer to an int 
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
