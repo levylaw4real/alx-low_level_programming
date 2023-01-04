ed6e916c77a9f2a091438f76b780e8e5061277ab
xylevy <82037387+xylevy@users.noreply.github.com>
2023-01-03 17:58:49 -0800

    Fixes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  *_memset - fills memory with a constant byte.
- *fills the first n bytes of the memory area 
- * pointed to by s with the constant b.
+ *
  *@s: pointer to memory area.
  *@b: constant to fill memory with.
  *@n: number of bytes of the memory area to be filled.
