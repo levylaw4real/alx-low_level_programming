3d0a16726d0312a9e6f2933f816609f27e3b1d7e
xylevy <82037387+xylevy@users.noreply.github.com>
2023-01-06 18:03:41 -0800

    Tasks

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - finds the factorial of a given number
+ * @n: int to calculate factorial
+ * Return: the factorial of @n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	else if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
