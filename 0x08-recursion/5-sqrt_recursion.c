b1b7370976082345c7c6135b8053a30929080054
xylevy <82037387+xylevy@users.noreply.github.com>
2023-01-06 18:20:47 -0800

    Task 5 and 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * helper - brutes/recurses to find sqroot of number
+ * @a: number
+ * @b: number to test for the square root of @a
+ * Return: square root
+ */
+int sqrt_helper(int a, int b)
+{
+	if (b * b > a)
+		return (-1);
+
+	else if (b * b == a)
+		return (b);
+
+	else
+		return (sqrt_helper(a, b + 1));
+
+	return (1);
+}
+/**
+ * _sqrt_recursion - finds the square root of a number
+ * @n: int
+ * Return: square root of int @n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqrt_helper(n, 1));
+}
