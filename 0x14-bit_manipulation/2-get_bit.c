8beb5f42cbaf29236a2da8f970c72219e6d944dd
xylevy <82037387+xylevy@users.noreply.github.com>
2023-02-07 19:02:55 -0800

    Fixees

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,6 +7,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8) return -1;
-    return (n & (1UL << index)) != 0;
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	return (n & (1UL << index)) != 0;
 }
