591ddc9eaa126b84304d7228bbbdadd1ef8a49b6
xylevy <82037387+xylevy@users.noreply.github.com>
2023-02-07 18:57:02 -0800

    Fixes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,28 @@
 #include "main.h"
 /**
-  * binary_to_uint - converts a binary number to an unsigned int
-  * @b: binary number
-  * Return: the converted number or 0 if it fails
-  */
+	* binary_to_uint - converts a binary number to an unsigned int
+	* @b: binary number
+	* Return: the converted number or 0 if it fails
+	*/
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int uint = 0;
+		unsigned int uint = 0;
 
-    if (!b) return 0;
+		if (!b)
 
-    while (*b)
-    {
-        if (*b == '0' || *b == '1')
-        {
-            uint = (uint << 1) | (*b - '0');
-            b++;
-        }
-        else return 0;
-    }
+			return (0);
 
-    return uint;
+		while (*b)
+		{
+			if (*b == '0' || *b == '1')
+			{
+					uint = (uint << 1) | (*b - '0');
+					b++;
+			}
+			else
+
+				return (0);
+		}
+
+		return (uint);
 }
