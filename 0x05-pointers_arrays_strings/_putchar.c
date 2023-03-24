b3109bb20f8c77e5a85cc26b9b1b8c3990189aef
xylevy <82037387+xylevy@users.noreply.github.com>
2023-03-23 20:03:02 -0700

    all

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,4 +1,5 @@
-#include <unistd.h>
+#include "shell.h"
+
 
 /**
  * _putchar - writes the character c to stdout
@@ -6,11 +7,18 @@
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- *
  */
-
 int _putchar(char c)
 {
-	
-	return (write(1, &c, 1));
+    static int i;
+    static char buf[WRITE_BUF_SIZE];
+
+    if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+    {
+        write(1, buf, i);
+        i = 0;
+    }
+    if (c != BUF_FLUSH)
+        buf[i++] = c;
+    return (1);
 }
